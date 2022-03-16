76d279e089cbfba0b0f1cde16b0c24d2891d914c
Haardispro <haardmajmudar2827@gmail.com>
2022-03-16 13:00:23 +0530

    Removed test.py project is done

--- main.py
+++ main.py
@@ -6,8 +6,9 @@ import time
 
 w = Tk()
 w.title("Rock Paper Scissors")
-#w.geometry("800x600")
+w.geometry("745x300")
 w.config(bg="#282828")
+w.resizable(width=False, height=False)
 
 #Fonts
 fonts = ("Cascadia Code", 24)
@@ -17,13 +18,14 @@ computer = random.choice(rps)
 print(computer)
 
 #Labels and headings
-you = Label(w, text="You", font=fonts, fg="white", bg="#282828")
-bot = Label(w, text="Computer", font=fonts, fg="white", bg="#282828")
-bot_choice = Label(w, text="Computer chose {}".format(computer), fg="white", bg="#282828", font=fonts)
+you = Label(w, text="You choose:", font=fonts, fg="white", bg="#282828")
+bot = Label(w, text="Computer chooses:", font=fonts, fg="white", bg="#282828")
+bot_choice = Label(w, text="{}".format(computer), fg="white", bg="#282828", font=fonts)
 
 win = Label(w, text="You win", fg="green", bg="#282828", font=fonts)
 lose = Label(w, text="You lose", fg="red", bg="#282828", font=fonts)
 draw = Label(w, text="Draw", fg="white", bg="#282828", font=fonts)
+
 
 #Functions for winning and losing
 def rock():
@@ -31,16 +33,16 @@ def rock():
     if computer == rps[0]:
         print("Draw")
         draw.grid(row=1, column=1, padx=10, pady=10)
+
     elif computer == rps[1]:
         print("Computer wins")
         lose.grid(row=1, column=1, padx=10, pady=10)
     elif computer == rps[2]:
         print("You win")
         win.grid(row=1, column=1, padx=10, pady=10)
-    #time.sleep(6)
-    #w.destroy()
-    #os.system("python another.py")
-    #computer = random.choice(rps)
+    rock['state'] = 'disabled'
+    paper['state'] = 'disabled'
+    scissors['state'] = 'disabled'
 
 def paper():
     bot_choice.grid(row=1, column=2, padx=10, pady=10)
@@ -53,9 +55,9 @@ def paper():
     elif computer == rps[2]:
         print("Computer wins")
         lose.grid(row=1, column=1, padx=10, pady=10)
-    #time.sleep(6)
-    #w.destroy()
-    #os.system("python another.py")
+    rock['state'] = 'disabled'
+    paper['state'] = 'disabled'
+    scissors['state'] = 'disabled'
 
 def scissors():
     bot_choice.grid(row=1, column=2, padx=10, pady=10)
@@ -68,20 +70,28 @@ def scissors():
     elif computer == rps[2]:
         print("Draw")
         draw.grid(row=1, column=1, padx=10, pady=10)
-    #time.sleep(6)
-    #w.destroy()
-    #os.system("python another.py")
+    rock['state'] = 'disabled'
+    scissors['state'] = 'disabled'
+    paper['state'] = 'disabled'
+    
+def restart():
+    w.destroy()
+    os.system("python main.py")
 
 #Three buttons for rock paper and scissors
 rock = Button(w, text="Rock", font=fonts, command=rock)
 paper = Button(w, text="Paper", font=fonts, command=paper)
 scissors = Button(w, text="Scissors", font=fonts, command=scissors)
 
+#Restart Button
+restart = Button(w, text="Reset", font=fonts, command=restart)
+
 #Positions
 #Buttons
 rock.grid(row=1, column=0, padx=10, pady=10)
 paper.grid(row=2, column=0, padx=10, pady=10)
 scissors.grid(row=3, column=0, padx=10, pady=10)
+restart.grid(row=3, column=1, padx=10, pady=10)
 #label
 you.grid(row=0, column=0, padx=10, pady=10)
 bot.grid(row=0, column=2, padx=10, pady=10)
